9b1f94a7ce2f17bfa95df26bef7a308facdd531b
umangkshah <umang.k.shah@gmail.com>
2016-11-13 09:35:01 -0600

    Client Side JSP and Servlet Pages
    Runs on seperate server from Web Services

---
+++ ansage_client/src/LoginService.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginService
+ */
+@WebServlet("/LoginService")
+public class LoginService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String username = request.getParameter("emailid");
+		//String pass = request.getParameter("passwd");
+		/*
+		 * LoginBean lb= new LoginBean();
+		 * lb.uname = username;
+		 * lb.password = pass;
+		 * web svc call (localhost:8080/ansage/doLogin/) 
+		 */
+		HttpSession s = request.getSession();
+		s.setAttribute("USER",username);
+		response.sendRedirect("index.jsp");
+	}
+
+}
