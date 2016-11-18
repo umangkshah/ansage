439be40b3627ee0d0978da025dd2217718aa4f62
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-17 19:07:20 -0600

    change

--- webSvcs/src/webSvcs/hibernatemethod.java
+++ webSvcs/src/webSvcs/hibernatemethod.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 import java.util.*;
 import org.hibernate.*;
 import org.json.simple.JSONObject;
+import org.json.simple.*;
 
 
 
@@ -17,7 +18,9 @@ public class hibernatemethod {
     private static SessionFactory sessionfactory;
     
    
-	public static String registration(JSONObject regdata)
+    
+   
+	public  hibernatepojo registration(JSONObject regdata)
 	{
 		
 		try
@@ -29,12 +32,7 @@ public class hibernatemethod {
 			System.err.println("Failed to create sessionFactory object." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
-		
-		
-		
-		
-	   
-	    String username=regdata.get("username").toString();
+		String username=regdata.get("name").toString();
 		String email=regdata.get("email").toString();
 		String tagline=regdata.get("tagline").toString();
 		String bio=regdata.get("bio").toString();
@@ -42,8 +40,8 @@ public class hibernatemethod {
 		String con=regdata.get("coins").toString();
 		String skills=regdata.get("skills").toString();
 		int coins=Integer.parseInt(con);
-		hibernatepojo reg=new hibernatepojo();
 		
+		hibernatepojo reg=new hibernatepojo();
 		login logdet=new login();
 		logdet.setEmail(email);
 		logdet.setPassword(password);
@@ -52,10 +50,8 @@ public class hibernatemethod {
 		reg.setEmail(email);
 		reg.setTagline(tagline);
 		reg.setBioinfo(bio);
-	     reg.setCoins(coins);
-	     
-		
-		Session session=sessionfactory.openSession();
+	    reg.setCoins(coins);
+	     Session session=sessionfactory.openSession();
 		Transaction tx=null;
 		try
 		{
@@ -67,22 +63,40 @@ public class hibernatemethod {
 		catch(Exception e)
 		{
 			if(tx!=null)
-				tx.rollback();
+			tx.rollback();
 			e.printStackTrace();
-				return "Failed";
+			return null;
+			
 		}
 		finally
 		{
 			session.close();
 		}
-	
-		
-		
-		
+		try
+		{
+		session=sessionfactory.openSession();
+		tx=session.beginTransaction();
+		@SuppressWarnings("rawtypes")
+		Query query=session.createQuery("from hibernatepojo where username=:username");
+		query.setParameter("username",username);
+		reg=(hibernatepojo)query.uniqueResult();
+		tx.commit();
+		if(reg==null)
+		return null;
+		}
+		catch(HibernateException e)
+		{
+				if(tx!=null)
+				tx.rollback();
+				e.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+		}
 		sessionfactory.close();
-		return "success";
-		
-}
+		return reg;
+		}
 	
 	@SuppressWarnings("deprecation")
 	public  hibernatepojo  login(JSONObject logindet)
@@ -120,6 +134,10 @@ public class hibernatemethod {
 		     prof=(login)query.uniqueResult();
 		     
 		     tx.commit();
+		     if(prof==null)
+		    	 return null;
+		     
+		     
 		}
 		catch(HibernateException e)
 		{
@@ -144,6 +162,9 @@ public class hibernatemethod {
 		query.setParameter("profileid",profileid);
 		reg=(hibernatepojo)query.uniqueResult();
 		tx.commit();
+		if(reg==null)
+			return null;
+		
 		
 		}
 		catch(HibernateException e)
@@ -165,4 +186,109 @@ public class hibernatemethod {
 		return reg;
 	}
 	
+	
+	public String usercheck(String email)
+	{  
+		hibernatepojo check=null;
+		try
+		{
+		 sessionfactory=new Configuration().configure().buildSessionFactory();
+		}
+		catch(Throwable ex)
+		{
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		Session session=sessionfactory.openSession();
+		Transaction tx=null;
+		try
+		{
+			tx=session.beginTransaction();
+		     @SuppressWarnings("rawtypes")
+		     Query query=session.createQuery("from hibernatepojo where email=:email");
+		     query.setParameter("email",email);
+		    check =(hibernatepojo)query.uniqueResult();
+		    tx.commit();
+		    if(check==null)
+		    	return "false";
+		    	
+		    
+		}
+		catch(HibernateException e)
+		{
+			if(tx!=null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally
+		{
+		session.close();
+			
+		}
+		
+		return "true";
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static  void retrieve()
+	{
+		JSONArray jsonobj=new JSONArray();
+		
+		try
+		{
+		 sessionfactory=new Configuration().configure().buildSessionFactory();
+		}
+		catch(Throwable ex)
+		{
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		Session session=sessionfactory.openSession();
+		Transaction tx=null;
+		try
+		{
+			tx=session.beginTransaction();
+		     @SuppressWarnings("rawtypes")
+		     Query query=session.createQuery("from hibernatepojo");
+		     List<hibernatepojo> reglist=query.list();
+		     tx.commit();
+		     for(hibernatepojo u:reglist)
+		     {
+		    	 JSONObject jobj=new JSONObject();
+				String name=u.getName();
+				String email=u.getEmail();
+				String tagline=u.getTagline();
+				String bio=u.getBioinfo();
+				int con=u.getCoins();
+				String skills=u.getSkills();
+				jobj.put("name", name);
+				jobj.put("email", email);
+				jobj.put("tagline", tagline);
+				jobj.put("bio", bio);
+				jobj.put("con", con);
+				jobj.put("skills", skills);
+				jsonobj.add(jobj);
+		     }
+				
+		}
+		catch(Throwable ex)
+		{
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		finally
+		{
+		session.close();
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
 }
+
+
