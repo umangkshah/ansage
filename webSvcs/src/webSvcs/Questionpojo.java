a1b8fe3e4c06e32230ea7aa9c891a37bccd5758d
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-18 22:53:28 -0600

    change

---
+++ webSvcs/src/webSvcs/Questionpojo.java
@@ -0,0 +1,75 @@
+package webSvcs;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.*;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.SecondaryTables;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.TemporalType;
+import org.hibernate.SessionFactory;
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+@Entity
+@Table(name="question")
+public class Questionpojo {
+	@Id
+	private int ownerid;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int qid;
+	private String ques;
+	private String desc;
+	private String category;
+	
+	public int getOwnerid() {
+		return ownerid;
+	}
+	public void setOwnerid(int ownerid) {
+		this.ownerid = ownerid;
+	}
+	public int getQid() {
+		return qid;
+	}
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+	public String getQues() {
+		return ques;
+	}
+	public void setQues(String ques) {
+		this.ques = ques;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+	
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
