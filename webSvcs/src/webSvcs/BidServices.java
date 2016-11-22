2fab8eb7576f73601c11b04457a18c0652fb2878
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-22 11:50:52 -0600

    bidservice

--- webSvcs/src/webSvcs/BidServices.java
+++ webSvcs/src/webSvcs/BidServices.java
@@ -1,14 +1,16 @@
 package webSvcs;
 
+
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
+import java.util.*;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
@@ -19,7 +21,7 @@ public class BidServices {
 	@POST
 	@Consumes(MediaType.TEXT_PLAIN)
 	public Response saveBid(String jon)
-	{
+		{
 		JSONParser parser = new JSONParser();
 		JSONObject json = new JSONObject();
 		try 
@@ -41,5 +43,24 @@ public class BidServices {
 			{
 			return Response.status(200).entity(check).build();
 			}
-	}
+		}
+	@SuppressWarnings("unchecked")
+	@Path("/retrieve")
+	@Produces(MediaType.APPLICATION_JSON)
+    public Response retbid(@PathParam("bid") String bd)
+    	{
+		  
+		  BidClass bid=new BidClass();
+		  List<Bidpojo> bidlist=bid.retbids(bd);
+		  GenericEntity<List<Bidpojo>> entity=new GenericEntity<List<Bidpojo>>(bidlist) {};
+		  
+		  if(bidlist==null)
+			  return Response.status(202).entity("false").build();
+		  else
+		  {
+		      return  Response.ok(entity).build();
+		  }
+		}
+			
 }
+	
