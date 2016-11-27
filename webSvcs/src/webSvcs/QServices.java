0c1cd4b426553ddd0ab5bd1d5f6b9a0bfc9ff516
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-26 20:53:11 -0600

    displayquestion

--- webSvcs/src/webSvcs/QServices.java
+++ webSvcs/src/webSvcs/QServices.java
@@ -4,11 +4,14 @@ import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
@@ -54,5 +57,38 @@ public class QServices {
 		JSONObject jo = qc.retrievequs(qid); 
 		return Response.status(200).entity(jo.toString()).build();
 	}
+	
+	@Path("/displayq")
+	public Response displayQuestion()
+	{
+		QuestionClass qc = new QuestionClass();
+		List<JSONObject> jsonlist=null;
+		List<Questionpojo> quslist=qc.displayqus();
+		if(quslist==null)
+		{
+			return Response.status(202).entity("false").build();
+		}
+		else
+		{
+			for(Questionpojo q:quslist)
+			{
+				JSONObject json=new JSONObject();
+				json.put("qid",q.getQid());
+				json.put("mainQ",q.getQus());
+				json.put("descrQ",q.getDescr());
+				json.put("ownerid",q.getOwnerid());
+				jsonlist.add(json);
+			}
+			
+			 
+			 return Response.status(200).entity(jsonlist).build();
+			
+		}
+		
+		
+		
+		
+	}
+	
 }
 
