03392e21d6e7d5a030736ba57f527fd71e05380a
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-29 22:43:30 -0600

    cart retrieval

--- webSvcs/src/webSvcs/TransClass.java
+++ webSvcs/src/webSvcs/TransClass.java
@@ -13,7 +13,6 @@ import org.json.simple.*;
 public class TransClass {
 	
 	private static SessionFactory sessionfactory;
-	
 	public String inscart(String bdid)
 	{
 		Session session=null;
@@ -163,6 +162,102 @@ public class TransClass {
 			session.close();
 		}
 		return "true";
+		
+	}
+	
+	public List<JSONObject> retcart(String ownerid)
+	{
+		Session session=null;
+		Transaction tx=null;
+		List<Transpojo> translist=new ArrayList<Transpojo>();
+		List<JSONObject>jsonlist=new ArrayList<JSONObject>();
+		Bidpojo bd=new Bidpojo();
+		Registrationpojo rd=new Registrationpojo();
+		int oid=Integer.parseInt(ownerid);
+		try
+		{
+		 sessionfactory=new Configuration().configure().buildSessionFactory();
+		}
+		catch(Exception ex)
+		{
+			return null;
+		}
+		try
+		{
+			session=sessionfactory.openSession();
+			tx=session.beginTransaction();
+			Query query=session.createQuery("from Transpojo WHERE oid=:oid");
+			query.setParameter("oid", oid);
+			translist=(List<Transpojo>)query.list();
+			tx.commit();
+		}
+		catch(HibernateException e)
+		{
+			if(tx!=null)
+			tx.rollback();
+			return null;
+			
+		}
+		finally
+		{
+			session.close();
+		}
+		for(Transpojo td:translist)
+		{
+			JSONObject json=new JSONObject();
+			int bidid=td.getBidid();
+			int profileid=td.getReqid();
+			try
+			{
+				session=sessionfactory.openSession();
+				tx=session.beginTransaction();
+				Query query=session.createQuery("from Bidpojo WHERE bidid=:bidid");
+				query.setParameter("bidid", bidid);
+				bd=(Bidpojo)query.uniqueResult();
+				tx.commit();
+			}
+			catch(HibernateException e)
+			{
+				if(tx!=null)
+				tx.rollback();
+				return null;
+				
+			}
+			finally
+			{
+				session.close();
+			}
+			int offer=bd.getOffer();
+			try
+			{
+				session=sessionfactory.openSession();
+				tx=session.beginTransaction();
+				Query query=session.createQuery("from Registrationpojo WHERE profileid=:profileid");
+				query.setParameter("profileid", profileid);
+				rd=(Registrationpojo)query.uniqueResult();
+				tx.commit();
+			}
+			catch(HibernateException e)
+			{
+				if(tx!=null)
+				tx.rollback();
+				return null;
+				
+			}
+			finally
+			{
+				session.close();
+			}
+			String name=rd.getName();
+			json.put("bidid",td.getBidid());
+			json.put("qty", td.getQty());
+			json.put("reqid",td.getReqid());
+			json.put("rname",rd.getName());
+			json.put("offer",bd.getOffer());
+			jsonlist.add(json);
+		}
+		sessionfactory.close();
+		return jsonlist;
 		
 	}
 	
