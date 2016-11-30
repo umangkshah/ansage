344da24e6271d3ae95be016344ade10f8d0207dd
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-11-29 20:35:27 -0600

    transaction

---
+++ webSvcs/src/webSvcs/Transpojo.java
@@ -0,0 +1,62 @@
+package webSvcs;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.*;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.SecondaryTables;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.TemporalType;
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+
+@Entity
+@Table(name="cart")
+
+public class Transpojo {
+	@Id
+	private int bidid;
+	private int qid;
+	private int reqid;
+	private int oid;
+	private int qty;
+	public int getBidid() {
+		return bidid;
+	}
+	public void setBidid(int bidid) {
+		this.bidid = bidid;
+	}
+	public int getQid() {
+		return qid;
+	}
+	public void setQid(int qid) {
+		this.qid = qid;
+	}
+	public int getReqid() {
+		return reqid;
+	}
+	public void setReqid(int reqid) {
+		this.reqid = reqid;
+	}
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	
+	
+	
+
+}
