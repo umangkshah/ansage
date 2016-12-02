996290cc2cb4af10f95c1d2678cb3142874cae5c
Sreenivas Venkitachalam <sxv163530@utdallas.edu>
2016-12-02 17:23:37 -0600

    cache

--- webSvcs/src/webSvcs/Answerpojo.java
+++ webSvcs/src/webSvcs/Answerpojo.java
@@ -11,12 +11,19 @@ import javax.persistence.SecondaryTables;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Temporal;
 
 
 @Entity
+@Cacheable
+@Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
 @Table(name="answer")
 public class Answerpojo {
 	@Id
